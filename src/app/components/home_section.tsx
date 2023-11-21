3e9be3da1bf115503b33909daae9c660f15778aa
Chingiz Topchiyev <cingiz.topciyev@btarge.com>
2023-11-21 23:09:27 +0400

    compeated home page

--- src/app/components/home_section.tsx
+++ src/app/components/home_section.tsx
@@ -1,19 +1,47 @@
 import classes from '../Scss/home.module.scss'
 const home_section = () => {
- 
+
   const arr = [
-     1, 'hello', 3, 4, 5, 6, 7, 8, 9, 10
-  ] 
+    'test', 'test', 'test', 'test', 'test', 'test',
+    'test', 'test', 'test', 'test', 'test', 'test',
+  ]
 
   return (
     <div className={classes.main_container}>
-      <div className={classes.ui_component}>
-        <ul>
-         {arr?.map((element, index) => <li key={index}>
-             {element}
-         </li>)}
-        </ul>
-      </div> 
+      <div className={classes.central_container}>
+        <h6>Categories</h6>
+        <div className={classes.central_container}>
+          <ul>
+            {arr?.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={classes.preseneter}>
+          <h6>Most Popular</h6>
+          <div>
+            <div>
+              {arr.map((item, index) => (
+                <div className={classes.cart} key={index}>
+                    {item}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={classes.preseneter}>
+          <h6>Most Popular</h6>
+          <div>
+            <div>
+              {arr.map((item, index) => (
+                <div className={classes.cart} key={index}>
+                    {item}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
