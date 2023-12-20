957fa2810f66f81721cab2e4189353cf5beba474
Andriy Lazunin <lazuninandriy@gmail.com>
2023-12-20 17:20:05 +0000

    20.55 14/09 update project

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './app/App';
 import {BrowserRouter} from "react-router-dom";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <BrowserRouter>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
+
             <App/>
-        </DevSupport>
+
     </BrowserRouter>
 );
 
